c85922c44b0a589032d3b269774241f4c3d4e2d6
megha <megha.patel2@sheridancollege.ca>
2019-04-10 09:26:41 -0400

    Commit 1

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week2.softwarefundamentals;
-
+//Test code added
 /**
  * A class to model Card Objects.
  * Created in Week 2 for the cloning repository demo
